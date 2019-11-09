9498ac65bdf9534d86ac4bdbab6dce93c383de40
saebido <sebido@ftwfoundation.org>
2019-11-09 08:09:12 +0800

    exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,15 @@
+#Variables, Strings, Ints and Prints Exercise
+
+#Given two variables name and age
+#Use the format () function to create a
+#sentence that reads:
+#Hi my name is Julie and I am 42 years old"
+
+#Set that equal to variable called
+#sentence
+
+name = "Julie"
+age = "42"
+sentence = "Hi my name is {} and I am {} years old".format(name, age)
+
+print(sentence)
